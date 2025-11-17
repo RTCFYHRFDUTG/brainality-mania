1f349d93fcc407af1ec62af111a34db637136371
RTCFYHRFDUTG <JAIDJAIDEN94@GMAIL.COM>
2025-11-16 16:35:55 -0800

    k

---
+++ docs/setup/windows.bat
@@ -0,0 +1,23 @@
+@echo off
+
+haxelib install flixel 5.5.0
+haxelib set flixel 5.5.0
+
+
+haxelib install flixel-addons 3.2.1
+haxelib set flixel-addons 3.2.1
+
+haxelib install flixel-demos 3.3.0
+haxelib set flixel-demos 3.3.0
+
+haxelib install flixel-templates 2.7.0
+haxelib set flixel-templates 2.7.0
+
+haxelib install flixel-tools 1.5.1
+haxelib set flixel-tools 1.5.1
+
+haxelib install flixel-ui 2.5.0
+haxelib set flixel-ui 2.5.0
+
+echo Done installing all libraries.
+pause
