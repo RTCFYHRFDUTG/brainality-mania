c7e8774d37e0c8b1b3529a448dad3c0adcaf440f
RTCFYHRFDUTG <JAIDJAIDEN94@GMAIL.COM>
2025-11-26 12:49:47 -0800

    Update Paths.hx

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -3,13 +3,14 @@ package backend;
 import haxe.Json;
 import sys.io.File;
 import sys.FileSystem;
+
 class Paths
 {
-    public static function getImage(path:String, parentFolder='shared')
+    public static function getImage(path:String, parentFolder = 'shared')
         return 'assets/${parentFolder}/${path}';
 
-    public static function getSong(path):String
-        return 'assets/songs${path}';
+    public static function getSong(path:String):String
+        return 'assets/songs/${path}';
 
     public static function getChart(songName:String, ?fileName:String = 'chart'):String
         return 'assets/data/${songName}/${fileName}.json';
@@ -17,5 +18,12 @@ class Paths
     public static function getJsonData(path:String):Dynamic
     {
         return Json.parse(File.getContent(path));
+    }
+
+    
+    public static function getVideo(path:String, parentFolder:String = 'videos'):String
+    {
+        
+        return 'assets/${parentFolder}/${path}.mp4';
     }
 }
